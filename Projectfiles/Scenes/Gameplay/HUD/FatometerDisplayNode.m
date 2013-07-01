b54f3d12b874efe9b7406b54fed74753b55f9577
Shalin Shah <shalinvs@gmail.com>
2013-07-01 16:35:43 -0700

    Revert "Deleted a few files and did some more changes"
    This reverts commit 359e866b6f850422ba26b05a5a90bf96a192ddfd.

---
+++ Projectfiles/Scenes/Gameplay/HUD/FatometerDisplayNode.m
@@ -0,0 +1,15 @@
+//
+//  FatometerDisplayNode.m
+//  My-MGWU-Endless-Game-Project
+//
+//  Created by Shalin Shah on 6/27/13.
+//  Copyright (c) 2013 MakeGamesWithUs Inc. All rights reserved.
+//
+
+#import "FatometerDisplayNode.h"
+
+@implementation FatometerDisplayNode
+
+@synthesize weight= _weight;
+
+@end
