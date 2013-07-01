58c89136372a6179ca90ec1376c9bf9cf9fec6d2
Shalin Shah <shalinvs@gmail.com>
2013-07-01 15:44:38 -0700

    Merged some chages

--- Projectfiles/Scenes/Gameplay/Objects/Monsters/Food.m
+++ Projectfiles/Scenes/Gameplay/Objects/Monsters/Food.m
@@ -10,7 +10,7 @@
 
 @implementation Food
 
-- (id)initWithUnhealthyPicture
+- (id)initWithMonsterPicture
 {
     self = [super initWithSpriteFrameName:@"monster1_1.png"];
     
@@ -126,62 +126,6 @@
 //    fatness = fatness + 10;
 //    fatness++;
     
-}
-
-- (void)unhealthyFoodGotCollected {
-    CCParticleSystem* system = [CCParticleSystemQuad particleWithFile:@"fx-explosion.plist"];
-    
-    // Set some parameters that can't be set in Particle Designer
-    system.positionType = kCCPositionTypeFree;
-    system.autoRemoveOnFinish = YES;
-    system.position = self.position;
-    
-    // Add the particle effect to the GameScene, for these reasons:
-    // - self is a sprite added to a spritebatch and will only allow CCSprite nodes (it crashes if you try)
-    // - self is now invisible which might affect rendering of the particle effect
-    // - since the particle effects are short lived, there is no harm done by adding them directly to the GameScene
-    [[[GameMechanics sharedGameMechanics] gameScene] addChild:system];
-    
-    CCSprite *coinSprite = [CCSprite spriteWithFile:@"coin.png"];
-    coinSprite.position = self.position;
-    [[[GameMechanics sharedGameMechanics] gameScene] addChild:coinSprite];
-    CGSize screenSize = [[GameMechanics sharedGameMechanics] gameScene].contentSize;
-    CGPoint coinDestination = ccp(21, screenSize.height-27);
-    CCMoveTo *move = [CCMoveTo actionWithDuration:2.f position:coinDestination];
-    id easeMove = [CCEaseBackInOut actionWithAction:move];
-    
-    CCAction *movementCompleted = [CCCallBlock actionWithBlock:^{
-        // this code is called when the movement is completed, then we we want to clean up the coinSprite
-        coinSprite.visible = FALSE;
-        [coinSprite removeFromParent];
-        coinSprite.zOrder = MAX_INT -1;
-    }];
-    
-    CCSequence *coinMovementSequence = [CCSequence actions:easeMove, movementCompleted, nil];
-    
-    [coinSprite runAction: coinMovementSequence];
-    
-    // mark as unvisible and move off screen
-    self.visible = FALSE;
-    self.position = ccp(-MAX_INT, 0);
-    [[GameMechanics sharedGameMechanics] game].enemiesKilled += 1;
-    [[GameMechanics sharedGameMechanics] game].score += 50;
-}
-
-- (void)spawn
-{
-	// Select a spawn location just outside the right side of the screen, with random y position
-	CGRect screenRect = [[CCDirector sharedDirector] screenRect];
-	CGSize spriteSize = [self contentSize];
-	float xPos = screenRect.size.width + spriteSize.width * 0.5f;
-	float yPos = CCRANDOM_0_1() * (0.25 * screenRect.size.height - spriteSize.height) + spriteSize.height * 0.5f;
-	self.position = CGPointMake(xPos, yPos);
-	
-	// Finally set yourself to be visible, this also flag the enemy as "in use"
-	self.visible = YES;
-	
-	// reset health
-	self.hitPoints = self.initialHitPoints;
 }
 
 @end
