18a60a9ac5386f5653e1d6133aaa70f65caa317d
Shalin Shah <shalinvs@gmail.com>
2013-07-01 15:29:20 -0700

    Revert "Revert "Revert "IDK"""
    This reverts commit 992db840d462ccb00b7949d53e80c2160f096bc7.

--- Projectfiles/Scenes/Gameplay/Objects/Monsters/Food.h
+++ Projectfiles/Scenes/Gameplay/Objects/Monsters/Food.h
@@ -10,13 +10,17 @@
 
 @interface Food : CCSprite
 
+{
+    float fatness;
+    bool healthy;
+}
+
 @property (nonatomic, assign) BOOL visible;
 
 - (id)initWithMonsterPicture;
 - (void)spawn;
 - (void)gotCollected;
 
-<<<<<<< HEAD
 @property (nonatomic, assign) NSInteger hitPoints;
 // velocity in pixels per second
 @property (nonatomic, assign) CGPoint velocity;
@@ -25,6 +29,4 @@
 @property (nonatomic, strong) CCAction *run;
 @property (nonatomic, assign) NSInteger initialHitPoints;
 
-=======
->>>>>>> parent of 604b170... Might as well commit before (possibly) breaking everything
 @end
