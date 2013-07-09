4221321a4020b624221d03309407472bc29ede33
Shalin Shah <shalinvs@gmail.com>
2013-07-09 13:22:40 -0700

    Added flying bananas
    Flying Bananas work :)

--- Projectfiles/Scenes/Gameplay/Objects/Monsters/MyCustomMonster.h
+++ Projectfiles/Scenes/Gameplay/Objects/Monsters/MyCustomMonster.h
@@ -10,4 +10,12 @@
 
 @interface MyCustomMonster : Food
 
+@property (nonatomic, assign) NSInteger hitPoints;
+// velocity in pixels per second
+@property (nonatomic, assign) CGPoint velocity;
+
+@property (nonatomic, strong) NSMutableArray *animationFrames;
+@property (nonatomic, strong) CCAction *run;
+@property (nonatomic, assign) NSInteger initialHitPoints;
+
 @end
