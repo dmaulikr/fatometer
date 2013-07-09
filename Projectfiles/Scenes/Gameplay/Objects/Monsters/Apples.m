082ceaf7106c494a460ea1102afb4f8622ffe176
Shalin Shah <shalinvs@gmail.com>
2013-07-09 14:12:25 -0700

    removed stuff

--- Projectfiles/Scenes/Gameplay/Objects/Monsters/Apples.m
+++ Projectfiles/Scenes/Gameplay/Objects/Monsters/Apples.m
@@ -17,36 +17,24 @@
     
     if (self)
     {
-        
-        
         self.initialHitPoints = 1;
-        // The line of code make the foods (a.k.a monsters) move towards the fast man (a.k.a knight).
-        //		self.velocity = CGPointMake(-30, 0);
-        
-        
         self.animationFrames = [NSMutableArray array];
-        
-        
-        
         //Create an animation from the set of frames you created earlier
         CCAnimation *running = [CCAnimation animationWithSpriteFrames: self.animationFrames delay:0.7f];
-        
         //Create an action with the animation that can then be assigned to a sprite
-        self.run = [CCRepeatForever actionWithAction: [CCAnimate actionWithAnimation:running]];
-        
+        self.run = [CCRepeatForever actionWithAction: [CCAnimate actionWithAnimation:running]];        
         // run the animation
         [self runAction:self.run];
         
         [self scheduleUpdate];
     }
-    
     return self;
 }
 
 
 - (void)spawn
 {
-    // Select a spawn location just outside the right side of the screen, with random y position
+    // Select a spawn location just outside the right side of the screen, and 
 	CGRect screenRect = [[CCDirector sharedDirector] screenRect];
 	CGSize spriteSize = [self contentSize];
 	float xPos = screenRect.size.width + spriteSize.width * 0.5f;
@@ -63,26 +51,17 @@
 {
     
     CCParticleSystem* system = [CCParticleSystemQuad particleWithFile:@"fx-explosion.plist"];
-    
     // Set some parameters that can't be set in Particle Designer
     system.positionType = kCCPositionTypeFree;
     system.autoRemoveOnFinish = YES;
     system.position = self.position;
-    
-    
-    // Add the particle effect to the GameScene, for these reasons:
-    // - self is a sprite added to a spritebatch and will only allow CCSprite nodes (it crashes if you try)
-    // - self is now invisible which might affect rendering of the particle effect
-    // - since the particle effects are short lived, there is no harm done by adding them directly to the GameScene
     [[[GameMechanics sharedGameMechanics] gameScene] addChild:system];
-    
     
     // mark as unvisible and move off screen
     self.visible = FALSE;
     self.position = ccp(-MAX_INT, 0);
     [[GameMechanics sharedGameMechanics] game].enemiesKilled += 1;
     [[GameMechanics sharedGameMechanics] game].score += 1;
-    
     [[GameMechanics sharedGameMechanics] game].fatness = [[GameMechanics sharedGameMechanics] game].fatness -= 8;
 }
 
