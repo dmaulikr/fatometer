6cb1e9b117648c615c942aee1f8b7912f6000d90
Shalin Shah <shalinvs@gmail.com>
2013-07-15 23:09:27 -0700

    Changed fatness bar frequency

--- Projectfiles/Scenes/Gameplay/Objects/Monsters/UnhealthyFood.m
+++ Projectfiles/Scenes/Gameplay/Objects/Monsters/UnhealthyFood.m
@@ -72,7 +72,7 @@
     // mark as unvisible and move off screen
     self.visible = FALSE;
     self.position = ccp(-MAX_INT, 0);
-    [[GameMechanics sharedGameMechanics] game].fatness += 25;
+    [[GameMechanics sharedGameMechanics] game].fatness += 9;
 //    [[GameMechanics sharedGameMechanics] game].enemiesKilled += 1;
 //    [[GameMechanics sharedGameMechanics] game].storeItems
 }
