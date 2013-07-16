43752bd8a2800136f62af60c965d610b917864a9
Shalin Shah <shalinvs@gmail.com>
2013-07-15 23:12:57 -0700

    Added Chicken

---
+++ Projectfiles/Scenes/Gameplay/Objects/Monsters/Chicken.m
@@ -0,0 +1,46 @@
+//
+//  Chicken.m
+//  Fat
+//
+//  Created by Shalin Shah on 7/15/13.
+//  Copyright (c) 2013 MakeGamesWithUs Inc. All rights reserved.
+//
+
+#import "Chicken.h"
+#import "GameMechanics.h"
+
+@implementation Chicken
+
+- (id)initWithMonsterPicture
+{
+    self = [super initWithFile:@"cake.png"];
+    
+    if (self)
+    {
+        self.initialHitPoints = 1;
+        self.animationFrames = [NSMutableArray array];
+        [self scheduleUpdate];
+    }
+    
+    return self;
+}
+
+
+- (void)gotCollected {
+    
+    CCParticleSystem* system = [CCParticleSystemQuad particleWithFile:@"fx-explosion.plist"];
+    // Set some parameters that can't be set in Particle Designer
+    system.positionType = kCCPositionTypeFree;
+    system.autoRemoveOnFinish = YES;
+    system.position = self.position;
+    [[[GameMechanics sharedGameMechanics] gameScene] addChild:system];
+    
+    // mark as unvisible and move off screen
+    self.visible = FALSE;
+    self.position = ccp(-MAX_INT, 0);
+    //    [[GameMechanics sharedGameMechanics] game].enemiesKilled += 1;
+    [[GameMechanics sharedGameMechanics] game].score += 1;
+    [[GameMechanics sharedGameMechanics] game].fatness = [[GameMechanics sharedGameMechanics] game].fatness += 6;
+}
+
+@end
