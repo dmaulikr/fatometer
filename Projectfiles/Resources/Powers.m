ad3e264eaa521ddfd73f69aaaa9d3f9793865495
Shalin Shah <shalinvs@gmail.com>
2014-07-18 14:33:32 -0700

    Revert 59b560d..761c3ab
    This rolls back to commit 59b560d740c123e2d634ccb69e4e77b2c238a646.

---
+++ Projectfiles/Resources/Powers.m
@@ -0,0 +1,14 @@
+//
+//  Powers.m
+//  FatGuyFred
+//
+//  Created by Shalin Shah on 3/8/14.
+//  Copyright (c) 2014 MakeGamesWithUs Inc. All rights reserved.
+//
+
+#import "Powers.h"
+#import "GameMechanics.h"
+
+@implementation Powers
+
+@end
